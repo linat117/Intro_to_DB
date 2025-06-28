e9d378865bff57717ebddda61ed8e811b6e935f3
Hilina Teshome <teshilina8@gmail.com>
2025-06-29 00:05:31 +0300

    ORder updated

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,8 +25,8 @@ CREATE TABLE Customers(
 CREATE TABLE Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
-    order_date DATE
+    order_date DATE,
+    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
